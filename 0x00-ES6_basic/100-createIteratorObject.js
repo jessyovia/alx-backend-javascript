5c8a4a48cc106f32509ece27d07d2471b91f6a95
jessyovia <jessyovia100@gmail.com>
2024-06-26 13:07:11 +0000

    all

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+// 100-createIteratorObject.js
+export default function createIteratorObject(report) {
+  let employees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    employees = employees.concat(department);
+  }
+  return employees[Symbol.iterator]();
+}
