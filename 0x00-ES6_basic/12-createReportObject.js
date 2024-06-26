62684dbf13fdb997573d084a12185fe3534c618d
jessyovia <jessyovia100@gmail.com>
2024-06-26 13:05:19 +0000

    all

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+// 12-createReportObject.js
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
