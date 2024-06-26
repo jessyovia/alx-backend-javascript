a50ad515274453194413f9e241ba5651d2e1ef58
jessyovia <jessyovia100@gmail.com>
2024-06-26 03:15:10 +0000

    Javascript

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,1 +1,2 @@
-import { taskFirst, taskNext } from './0-constants.js'; console.log(\`\${taskFirst()} \${taskNext()}\`);" > 0-main.js
+import { taskFirst, taskNext } from './0-constants.js';
+console.log(`${taskFirst()} ${taskNext()}`);
