ccc6667b6bf4f8ee1fa0ff2cbb8f45dc736a74c3
jessyovia <jessyovia100@gmail.com>
2024-06-26 13:08:18 +0000

    all

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+// 101-iterateThroughObject.js
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+  return employees.join(' | ');
+}
