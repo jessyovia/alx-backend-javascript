4a5fced472d4333fadf137c137d6332b41bc4608
jessyovia <jessyovia100@gmail.com>
2024-06-27 03:18:43 +0000

    all

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,4 @@
+// 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
+  return new Promise((resolve, reject) => {});
 }
