e1e5777e348b7485879e0aa159b363b93c0e8ca6
jessyovia <jessyovia100@gmail.com>
2024-06-27 03:26:18 +0000

    all

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+// 2-then.js
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
