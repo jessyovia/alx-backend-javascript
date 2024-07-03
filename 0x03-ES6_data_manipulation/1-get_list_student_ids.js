024f80c5ae9b52324f74d8d024aa754c67ff7792
jessyovia <jessyovia100@gmail.com>
2024-07-03 02:55:36 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-// 1-main.js
-import getListStudentIds from "./1-get_list_student_ids.js";
-import getListStudents from "./0-get_list_students.js";
-
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map(student => student.id);
+}
